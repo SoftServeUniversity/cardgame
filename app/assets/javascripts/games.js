202dd7027a7f3f94f74209e6789982a40090e633
Anton Udyrenko <stokipost@gmail.com>
2015-01-09 04:03:16 +0200

    HELPERS for drawing Cards ranks and suits. New info-panel in game. Get rid of info-column. Appeareance reworks. Work on JS effects.

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -16,5 +16,17 @@ $(function(){
 			return $(this).parents().find("#logInForm").html();
 		}
 	});
+	
+	$("#turnInfoButton").popover({
+		html: true,
+		trigger: "hover",
+		placement: "left",
+		animation: "true",
+		content: function(){
+			return $(this).parents().find("#turnInfoHint").html();
+		}
+	});
+
+
 
 });
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,62 @@
 module GamesHelper
+
+  def make_suite cardSuite
+    case cardSuite
+      when 'diamonds'
+        raw("<span class='redSuite'>&diams;</span>")
+      when 'spades'
+        raw("<span class='blackSuite'>&spades;</span>")
+      when 'hearts'
+        raw("<span class='redSuite'>&hearts;</span>")
+      else
+        raw("<span class='blackSuite'>&clubs;</span>")
+    end
+  end
+
+  def make_rank card
+  	if (card.suite == 'diamonds') || (card.suite == 'hearts')
+      case card.rang
+        when 0
+          raw("<span class='redSuite'>6</span>")
+        when 1
+          raw("<span class='redSuite'>7</span>")
+        when 2
+          raw("<span class='redSuite'>8</span>")
+        when 3
+          raw("<span class='redSuite'>9</span>")
+        when 4
+          raw("<span class='redSuite'>10</span>")
+        when 5
+          raw("<span class='redSuite'>J</span>")
+        when 6
+          raw("<span class='redSuite'>Q</span>")
+        when 7
+          raw("<span class='redSuite'>K</span>")
+        else
+          raw("<span class='redSuite'>A</span>")
+      end
+    elsif (card.suite == 'spades') || (card.suite == 'clubs')
+    	case card.rang
+        when 0
+          raw("<span class='blackSuite'>6</span>")
+        when 1
+          raw("<span class='blackSuite'>7</span>")
+        when 2
+          raw("<span class='blackSuite'>8</span>")
+        when 3
+          raw("<span class='blackSuite'>9</span>")
+        when 4
+          raw("<span class='blackSuite'>10</span>")
+        when 5
+          raw("<span class='blackSuite'>J</span>")
+        when 6
+          raw("<span class='blackSuite'>Q</span>")
+        when 7
+          raw("<span class='blackSuite'>K</span>")
+        else
+          raw("<span class='blackSuite'>A</span>")
+      end
+    end
+  end
+
 end
