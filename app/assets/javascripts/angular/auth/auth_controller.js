df133d58f3da786a60628e4c02f681b584ed4db0
Nazar Yablonskiy <nazaryablonskiy@gmail.com>
2015-01-12 12:55:25 +0200

    logout

--- app/assets/javascripts/angular/auth/auth_controller.js
+++ app/assets/javascripts/angular/auth/auth_controller.js
@@ -1,26 +1,22 @@
 var MyApp = angular.module("MyApp");
 
-MyApp.controller("AuthController", ["$scope", "Auth", "$location", "$http", function($scope, Auth, $location , $http){
-	$scope.signedIn = Auth.isAuthenticated();
-	$scope.login = function(){
-		Auth.login($scope.user).then(function(user){
-			$scope.signedIn = Auth.isAuthenticated();
-			$location.path('/home');
-		});
-	};
+MyApp.controller("AuthController", ["$scope", "Auth", "$location", "$http",
+    function($scope, Auth, $location, $http) {
+        $scope.signedIn = Auth.isAuthenticated();
 
-	$scope.logout = function(){
-		Auth.logout().then(function(){
-			$scope.signedIn = Auth.isAuthenticated();
-			$location.path('/home');
-		});
-	};
+        $scope.login = function() {
+            Auth.login($scope.user).then(function(user) {
+                $scope.signedIn = Auth.isAuthenticated();
+                $location.path('/home');
+            });
+        };
 
-	$scope.register = function(){
-		Auth.register($scope.user).then(function(user){
-			console.log(user);
-			$location.path('/home');
-		});
-	};
+        $scope.register = function() {
+            Auth.register($scope.user).then(function(user) {
+                console.log(user);
+                $location.path('/home');
+            });
+        };
 
-}]);
+    }
+]);
--- app/assets/javascripts/angular/nav/nav_controller.js
+++ app/assets/javascripts/angular/nav/nav_controller.js
@@ -4,6 +4,14 @@ MyApp.controller("NavController", ["$scope", "Auth", function($scope, Auth){
 	$scope.signedIn = Auth.isAuthenticated();
 	$scope.logout = Auth.logout();
 	
+	$scope.logout = function() {
+            Auth.logout().then(function(oldUser) {
+                alert(oldUser.username + "you're signed out now.");
+            }, function(error) {
+                console.log(error);
+            });
+        };
+	
 	Auth.currentUser().then(function(user){
 		$scope.signedIn = Auth.isAuthenticated();
 		$scope.user = user;
