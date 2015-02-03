180c632e86049636407db7be2b35e2f7a0e14f74
Kychun_O <kichun.oleksandr@gmail.com>
2015-02-04 00:14:25 +0100

    constants added to table_cpec

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -9,27 +9,27 @@ describe Table do
 
   describe  "#trump?" do
     it "should compare for trump" do
-      iterate_cards_for_trump(0)
+      iterate_cards_for_trump(NUMBER_ZERO)
 
-      iterate_cards_for_trump(1)
+      iterate_cards_for_trump(NUMBER_ONE)
     end
   end
 
   describe  "table_empty?" do
     it "should inspect table for emptyness" do
-      expect(@game.table.table_empty?).to eq(true)
+      expect(@game.table.table_empty?).to eq(TRUE)
     end
   end
 
   describe  "defend?" do
     it "should return bool if player now defender" do
-      expect_defend_or_attack("defend")
+      expect_defend_or_attack(DEFEND)
     end
   end
 
   describe  "attack?" do
     it "should return bool if player now attacker" do
-      expect_defend_or_attack("attack")
+      expect_defend_or_attack(ATTACK)
     end
   end
 
@@ -40,38 +40,38 @@ describe Table do
     it "should first initialize array with card" do
 
       expect(@game.table.table_cards).to_not eq([])
-      expect(@game.table.cards_count).to eq(1)
+      expect(@game.table.cards_count).to eq(NUMBER_ONE)
     end
     it "should clear array and counter" do
       @game.table.clear
 
       expect(@game.table.table_cards).to eq([])
-      expect(@game.table.cards_count).to eq(0)
+      expect(@game.table.cards_count).to eq(NUMBER_ZERO)
     end
   end
 
   describe  "put_cards" do
     it "should put_cards depending on mover" do
-      if move_of_player?("second")
-        create_and_put_card(1)
+      if move_of_player?(SECOND_PLAYER)
+        create_and_put_card(NUMBER_ONE)
 
-        expect(@game.table.put_cards[0]).to eq(@card1)
-      elsif move_of_player?("first")
-        create_and_put_card(0)
+        expect(@game.table.put_cards[NUMBER_ZERO]).to eq(@card1)
+      elsif move_of_player?(FIRST_PLAYER)
+        create_and_put_card(NUMBER_ZERO)
 
-        expect(@game.table.put_cards[0]).to eq(@card1)
+        expect(@game.table.put_cards[NUMBER_ZERO]).to eq(@card1)
       end
     end
   end
 
   describe  "get_latest_card" do
     it "should pick latest card on mover" do
-      if move_of_player?("second")
-        create_and_put_card(1)
+      if move_of_player?(SECOND_PLAYER)
+        create_and_put_card(NUMBER_ONE)
 
         expect(@game.table.get_latest_card).to eq(@card1)
-      elsif move_of_player?("first")
-        create_and_put_card(0)
+      elsif move_of_player?(FIRST_PLAYER)
+        create_and_put_card(NUMBER_ZERO)
 
         expect(@game.table.get_latest_card).to eq(@card1)
       end
@@ -80,26 +80,26 @@ describe Table do
 
   describe  "allow_attack?" do
     it "should alow attack" do
-      if move_of_player?("second")
-        expect(@game.table.allow_attack? @game.players[1].player_cards[0]).to eq(true)
-        loop_to_find_trump(1)
+      if move_of_player?(SECOND_PLAYER)
+        expect(@game.table.allow_attack? @game.players[NUMBER_ONE].player_cards[NUMBER_ZERO]).to eq(true)
+        loop_to_find_trump(NUMBER_ONE)
       else
-        expect(@game.table.allow_attack? @game.players[0].player_cards[0]).to eq(true)
-        loop_to_find_trump(0)
+        expect(@game.table.allow_attack? @game.players[NUMBER_ZERO].player_cards[NUMBER_ZERO]).to eq(true)
+        loop_to_find_trump(NUMBER_ZERO)
       end
     end
   end
 
   describe  "allow_defend?" do
     it "should alow defend" do
-      if move_of_player?("second")
-        create_and_put_card(1)
+      if move_of_player?(SECOND_PLAYER)
+        create_and_put_card(NUMBER_ONE)
 
-        loop_and_check_allow_defence(0)
+        loop_and_check_allow_defence(NUMBER_ZERO)
       else
-        create_and_put_card(0)
+        create_and_put_card(NUMBER_ZERO)
 
-        loop_and_check_allow_defence(1)
+        loop_and_check_allow_defence(NUMBER_ONE)
       end
     end
   end
@@ -108,25 +108,25 @@ describe Table do
     it "should push to the table" do
       iterate_and_push_cards
       loop_over_table_cards_check_presence
-      if @game.players[1] == @game.attacker
-        expect(@game.table.cards_count).to eq(4)
-        expect(@game.table.attacker_cursor).to eq(8)
+      if @game.players[NUMBER_ONE] == @game.attacker
+        expect(@game.table.cards_count).to eq(NUMBER_FOUR)
+        expect(@game.table.attacker_cursor).to eq(NUMBER_EIGHT)
       else
-        expect(@game.table.cards_count).to eq(4)
-        expect(@game.table.defender_cursor).to eq(9)
+        expect(@game.table.cards_count).to eq(NUMBER_FOUR)
+        expect(@game.table.defender_cursor).to eq(NUMBER_NINE)
       end
     end
   end
 
   describe  "add_card" do
     it "should check if possible to add_card  and push to table" do
-      if move_of_player?("second")
-        init_card_add_to_table_check_expectations(1) do
-          expect(@game.table.add_card(@game.players[0].player_cards[4],
-                                      @game.players[0], @game.attacker )).to eq(false)
+      if move_of_player?(SECOND_PLAYER)
+        init_card_add_to_table_check_expectations(NUMBER_ONE) do
+          expect(@game.table.add_card(@game.players[NUMBER_ZERO].player_cards[NUMBER_FOUR],
+                                      @game.players[NUMBER_ZERO], @game.attacker )).to eq(FALSE)
         end
-      elsif move_of_player?("first")
-        init_card_add_to_table_check_expectations(0)
+      elsif move_of_player?(FIRST_PLAYER)
+        init_card_add_to_table_check_expectations(NUMBER_ZERO)
       end
     end
   end
