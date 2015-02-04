7df0a12d865e3a6ec27ffe16a21a01ab957b9bd1
Kychun_O <kichun.oleksandr@gmail.com>
2015-02-04 21:05:37 +0100

    user validation test added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,28 @@ describe User do
   it "shouldn't have assosiation belongs_to Game" do
     expect(associate.macro).to_not  eq(:belongs_to)
   end
+  describe "validation uniqueness" do
+    before(:each) do
+      user = create(:user)
+      @username = user.username
+    end
 
+    it "should be valid if given right params" do
+      expect(user.valid?).to be(TRUE)
+    end
+
+    let(:user2){build(:user, username: @username)}
+
+    it "should not be valid if username taken" do
+      expect(user2.valid?).to be(FALSE)
+    end
+
+    it "should be valid if taken username upcase" do
+      user2.username.upcase!
+      expect(user2.valid?).to be(FALSE)
+    end
+
+  end
   describe "#after_initialize :init" do
     context "calling User.new triggers method init" do
 
@@ -25,14 +46,14 @@ describe User do
   end
 
   describe "#self.find_for_database_authentication" do
-    it "should invoke #dup and #delete 
+    it "should invoke #dup and #delete
         on given param and find user with given name" do
       setup_user_for_database_auth(NAME_SAMPLE)
 
       expect(User.find_for_database_authentication(@warden_condition)).to eq(@user)
     end
 
-    it "should invoke #dup and #delete 
+    it "should invoke #dup and #delete
         on given param and find user with given email" do
       setup_user_for_database_auth(NIL, EMAIL_SAMPLE)
 
@@ -41,7 +62,7 @@ describe User do
 
     it "should not find any user if name doesn't match" do
       setup_user_for_database_auth(WRONG_NAME_SAMPLE)
-      
+
       expect(User.find_for_database_authentication(@warden_condition)).to be_nil
     end
   end
